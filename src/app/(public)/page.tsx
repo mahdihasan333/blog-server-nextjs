ad92fa9ee336df4cdd4e20fa2a6a73945af6a6c4
Mahdi Hassan <mahdioffi333@gmail.com>
2025-10-04 18:24:20 +0600

    Caching and revalidating the latest blogs on the homepage

--- src/app/(public)/page.tsx
+++ src/app/(public)/page.tsx
@@ -1,10 +1,19 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import BlogCard from "@/components/modules/Blogs/BlogCard";
 import Hero from "@/components/modules/Home/Hero";
 
-export default function HomePage() {
+export default async function HomePage() {
+  const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_API}/post`);
+  const {data: blogs} = await res.json();
   return (
     <div>
       <Hero />
       <h2 className="text-center my-5 text-4xl">Featured Posts</h2>
+      <div className="grid grid-cols-3 gap-4">
+    {
+      blogs.map((blog: any) => <BlogCard key={blog?.id} post={blog} />)
+    }
+      </div>
     </div>
   );
 }
