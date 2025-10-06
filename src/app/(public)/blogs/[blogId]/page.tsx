724d8bab601a5301059adea24c75e530e2a7d99a
Mahdi Hassan <mahdioffi333@gmail.com>
2025-10-07 04:49:12 +0600

    Creating Blogs Using Next.js Form Component

--- src/app/(public)/blogs/[blogId]/page.tsx
+++ src/app/(public)/blogs/[blogId]/page.tsx
@@ -4,12 +4,13 @@ import React from 'react';
 
 export const generateStaticParams = async() => {
     const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_API}/post`);
-    const blogs = await res.json();
+    const { data: blogs} = await res.json();
 
     return blogs.slice(0, 2).map((blog: any) => ({
         blogId: String(blog._id)
     }))
 }
+
 
 
 export const generateMetadata = async({params} : {params: Promise<{blogId: string[]}>}) => {
