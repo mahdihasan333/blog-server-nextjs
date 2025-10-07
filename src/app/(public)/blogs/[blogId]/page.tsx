3d9d574d26faff79234e39be0c3f38d969c302ab
Mahdi Hassan <mahdioffi333@gmail.com>
2025-10-07 12:51:38 +0600

    optimized a function

--- src/app/(public)/blogs/[blogId]/page.tsx
+++ src/app/(public)/blogs/[blogId]/page.tsx
@@ -1,5 +1,6 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import BlogDetailsCard from '@/components/modules/Blogs/BlogDetailsCard';
+import { getBlogById } from '@/services/PostServices';
 import React from 'react';
 
 export const generateStaticParams = async() => {
@@ -13,11 +14,10 @@ export const generateStaticParams = async() => {
 
 
 
-export const generateMetadata = async({params} : {params: Promise<{blogId: string[]}>}) => {
+export const generateMetadata = async({params} : {params: Promise<{blogId: string}>}) => {
     const {blogId} = await params;
 
-    const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_API}/post/${blogId}`);
-    const blog = await res.json();
+    const blog = await getBlogById(blogId)
 
     return {
         title: blog?.title,
@@ -26,12 +26,11 @@ export const generateMetadata = async({params} : {params: Promise<{blogId: strin
 }
 
 
-const BlogDetailsPage = async ({params} : {params: Promise<{blogId: string[]}>}) => {
+const BlogDetailsPage = async ({params} : {params: Promise<{blogId: string}>}) => {
 
     const {blogId} = await params;
 
-    const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_API}/post/${blogId}`);
-    const blog = await res.json();
+    const blog = await getBlogById(blogId)
 
     return (
         <div className='py-30 px-4 max-w-7xl mx-auto'>
