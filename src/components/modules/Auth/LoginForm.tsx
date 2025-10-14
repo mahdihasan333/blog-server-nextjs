a6846711a73294760c1ff2b29b89e6b6f9345d12
Mahdi Hassan <mahdioffi333@gmail.com>
2025-10-14 15:47:04 +0600

    Implement NextAuth Credentials login system

--- src/components/modules/Auth/LoginForm.tsx
+++ src/components/modules/Auth/LoginForm.tsx
@@ -15,8 +15,8 @@ import {
 } from "@/components/ui/form";
 import Image from "next/image";
 import { signIn } from "next-auth/react";
-import { toast } from "sonner";
 import { login } from "@/actions/auth";
+import { toast } from "sonner";
 
 // type LoginFormValues = {
 //   email: string;
