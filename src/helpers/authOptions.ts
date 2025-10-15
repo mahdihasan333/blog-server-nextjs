078c6242a13c7fbf597eb29304af56c7878614ab
Mahdi Hassan <mahdioffi333@gmail.com>
2025-10-15 16:14:12 +0600

    Add user id to token with NextAuth callbacks

--- src/helpers/authOptions.ts
+++ src/helpers/authOptions.ts
@@ -76,5 +76,22 @@ export const authOptions: NextAuthOptions = {
       },
     }),
   ],
-  
+  callbacks: {
+    async jwt({ token, user }) {
+      if (user) {
+        token.id = user?.id;
+      }
+      return token;
+    },
+    async session({ session, token }) {
+      if (session?.user) {
+        session.user.id = token?.id as string;
+      }
+      return session;
+    },
+  },
+  secret: process.env.AUTH_SECRET,
+  pages: {
+    signIn: "/login",
+  },
 };
